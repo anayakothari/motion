0c8f823ef406668be28f8aa63b2ea3056e7c5515
anayakothari <anayakothari77@gmail.com>
2022-10-19 18:27:33 -0400

    animation

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,16 +1,32 @@
-import $ from "jquery";
+import {
+    gsap
+} from "gsap";
+import {
+    ScrollTrigger
+} from "gsap/ScrollTrigger";
 
-let $nav = $('nav[data-nav]');
-let isVisible = false;
+gsap.registerPlugin(ScrollTrigger);
 
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
+
+function scrollAnimation() {
+    var tl = gsap.timeline();
+    tl.to("#robotanimation", {
+        duration: 1,
+        x: 400,
+        rotation: 360,
+        scrollTrigger: {
+            trigger: "#robotanimation",
+            // toggleActions: "restart pause reverse pause",
+            markers: true,
+            start: "0px 50%",
+            end: "100% 10%",
+            scrub: true,
+            pin: true
+        }
+    });
+    return tl;
+}
+
+
+var mainTL = gsap.timeline();
+mainTL.add(scrollAnimation());
