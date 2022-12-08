e6194737e28c111a0a5241f49bb096662bc457c7
anayakothari <anayakothari77@gmail.com>
2022-12-08 03:01:04 -0500

    drawsvg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,17 +1,104 @@
-import { gsap } from "gsap";
-import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
-import { GSDevTools } from "gsap/GSDevTools";
+import {
+    gsap
+} from "gsap";
+import {
+    DrawSVGPlugin
+} from "gsap/DrawSVGPlugin";
+import {
+    GSDevTools
+} from "gsap/GSDevTools";
+import {
+    MorphSVGPlugin
+} from "gsap/MorphSVGPlugin";
 
-gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
+gsap.registerPlugin(DrawSVGPlugin, GSDevTools, MorphSVGPlugin);
 
 
-function boxAnimation(){
+function CustomLogoAnimation() {
     var tl = gsap.timeline();
-    tl.fromTo("#box",{drawSVG:"0% 5%"},{duration:1, fill:"red", drawSVG:"95% 100%"})
-    .from("#line",{duration:1, drawSVG:"0%"});
-    return tl;
-}
+    tl.from("#leftshape", {
+            drawSVG: "25% 100%",
+            duration: 1,
+        })
+        .from("#globe-stand-fill", {
+            opacity: 0,
+            delay: .25,
+            duration: 1
+        })
+        .from("#first-w", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#first-w-fill", {
+            opacity: 0,
 
+            duration: .25
+        })
+        .from("#second-w", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#second-w-fill", {
+            opacity: 0,
+
+            duration: .25
+        })
+        .from("#letter-a", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#letter-a-fill", {
+            opacity: 0,
+
+            duration: .25
+        })
+        .from("#letter-n", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#letter-n-fill", {
+            opacity: 0,
+
+            duration: .25
+        })
+        .from("#letter-d", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#letter-d-fill", {
+            opacity: 0,
+
+            duration: .25
+        })
+        .from("#letter-e", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#letter-e-fill", {
+            opacity: 0,
+
+            duration: .25
+        })
+
+        .from("#letter-r", {
+            drawSVG: "75% 100%",
+            scale: .75,
+            x: -200
+        })
+        .from("#letter-r-fill", {
+            opacity: 0,
+
+            duration: .25
+        });
+    return tl;
+
+}
 
 var mainTl = gsap.timeline();
 mainTl.add(boxAnimation());
