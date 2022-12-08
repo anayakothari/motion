98af110bf2a94745919443467e609236e96f5fff
anayakothari <anayakothari77@gmail.com>
2022-12-08 00:27:24 -0500

    drawsvg

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -11,20 +11,38 @@ import {
     MorphSVGPlugin
 } from "gsap/MorphSVGPlugin";
 
+
 function FoundLogoAnimation() {
     var tl = gsap.timeline();
     tl.from("#globe-stand", {
-        drawSVG: "75 % 75 %"
-    }, {
-        duration: 1,
-        drawSVG: "25% 125%"
-    })
+            drawSVG: "75 % 75 %"
+        }, {
+            duration: 1,
+            drawSVG: "25% 125%"
+        })
+        .from("#first-w", {
+            drawSVG: "50% 50%"
+        }, {
+            delay: 5,
+            duration: 1,
+            scale: .75,
+            x: -200
+        })
+        .from("#second-w", {
+            drawSVG: "50% 50%"
+        }, {
+            delay: 10,
+            duration: 1,
+            scale: .75,
+            x: -200
+        })
     return tl;
-}
 
+}
 
 var mainTl = gsap.timeline();
 mainTl.add(FoundLogoAnimation());
+
 
 gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
